875db73b4fe92f5085aa209c8ad42f8f22385e14
Eufrausino <gustavo.medeiros@ufv.br>
2025-01-18 10:21:09 -0300

    Subindo implementação final de tarefaB

--- Geral/Headers/includes.h
+++ Geral/Headers/includes.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <ctype.h>
 
 #define TAM_CAMINHO 60
 
--- Geral/Src/main.c
+++ Geral/Src/main.c
@@ -4,18 +4,48 @@
 
 int main()
 {
-	char nome[TAM_CAMINHO] = "Geral/Lib/teste.txt";
+	//TarefaB - Chave aleatória
+	srand(time(NULL));
+	int chaveAleatoria = (rand()%1000)+1;
+	printf("Chave Aleatória = %d\n", chaveAleatoria);
+	
+	//TarefaB - criptografia e descriptografia padrão
+	//Arquivo -> string
+	char nome[TAM_CAMINHO] = "Geral/Lib/test2.txt";
 	char* texto;
 	arquivoParaString(nome, &texto); 
-	printf("%s\n",texto);
-	criptografar(100, texto);
-	descriptografar(100, texto);
-	FILE* criptografado = fopen("Out/criptografia.txt","r");
-	FILE* descriptografado = fopen("Out/descriptografia.txt","r");
+	printf("oK\n");
+	
+	//Criptografia e descriptografia padrão
+	char criptografia[50];
+	char decriptografia[50];
+	printf("Entre com o nome do arquivo de criptografia sem extensão: ");
+	scanf("%s",criptografia);
+	printf("\nEntre com o nome do arquivo de decriptografia sem extensão: ");
+	scanf("%s",decriptografia);
+
+	char* crypto = criptografar(100, texto, criptografia);
+	char* decrypto = descriptografar(100, texto, decriptografia);
+
+	FILE* criptografado = fopen(crypto,"r");
+	FILE* descriptografado = fopen(decrypto,"r");
+
+	//Exibição do conteúdo do arquivo criptografado e descriptografado padrão
 	char cripto,decripto;
 	while((cripto = fgetc(criptografado))!= EOF)printf("%c",cripto);
 	printf("\n");
 	while((decripto=fgetc(descriptografado))!=EOF)printf("%c",decripto);
+	printf("ALEATORIO\n");	
+
+	//Chave aleatória
+	char KRYPTO[50];
+	printf("Entre com o nome do arquivo de criptografia sem extensão: ");
+	scanf("%s",KRYPTO);
+	adivinhaChave(chaveAleatoria, texto, KRYPTO);
+
+	//Liberação de memória
 	free(texto);
+	free(crypto);
+	free(decrypto);
 	return 0;
 }
